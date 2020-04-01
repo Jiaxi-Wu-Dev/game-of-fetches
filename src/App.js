6c0a9159997d6fa5804f82907ec66e12fc0f06c8
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-01 14:13:03 -0400

    Margerie's Birthdate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 import './App.css';
 import axios from 'axios'
 
+let tyrellOne = "https://anapioficeandfire.com/api/characters/16"
+let targaryenTwo = "http://www.anapioficeandfire.com/api/houses/378"
+let lannisterThree = "http://www.anapioficeandfire.com/api/houses/229"
+let baratheonFour = "http://www.anapioficeandfire.com/api/houses/17"
+let aliasFive = "http://www.anapioficeandfire.com/api/characters/901"
+let starkSix = "http://www.anapioficeandfire.com/api/houses/362"
+let booksSeven = "http://www.anapioficeandfire.com/api/characters/232"
+
+
+const requestOne = axios.get(one);
+
+requestOne.then(response => {
+  console.log(response)
+}).catch(error => {
+  console.err(error)
+})
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -28,14 +45,14 @@ export default class App extends Component {
         console.log('there is an eror', error)
       })
 
-      axios.get("https://anapioficeandfire.com/api/characters/16")
+      axios.get("http://www.anapioficeandfire.com/api/houses/378")
       // making a call for margaeryTyrells info
       // got a CORS error
       .then(res => {
-        const margaeryTyrell = res.data;
-        console.log("data ---->", margaeryTyrell)
+        const houseTargaryen = res.data;
+        console.log("data ---->", houseTargaryen)
 
-        this.setState({ data: margaeryTyrell })
+        this.setState({ data: houseTargaryen })
 
       })
       .catch(error => {
