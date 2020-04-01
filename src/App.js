6ab9d5765f02560db7a838057806792dc199d486
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-01 14:17:23 -0400

    targaryen

--- src/App.js
+++ src/App.js
@@ -2,22 +2,6 @@ import React, { Component } from 'react'
 import './App.css';
 import axios from 'axios'
 
-let tyrellOne = "https://anapioficeandfire.com/api/characters/16"
-let targaryenTwo = "http://www.anapioficeandfire.com/api/houses/378"
-let lannisterThree = "http://www.anapioficeandfire.com/api/houses/229"
-let baratheonFour = "http://www.anapioficeandfire.com/api/houses/17"
-let aliasFive = "http://www.anapioficeandfire.com/api/characters/901"
-let starkSix = "http://www.anapioficeandfire.com/api/houses/362"
-let booksSeven = "http://www.anapioficeandfire.com/api/characters/232"
-
-
-const requestOne = axios.get(one);
-
-requestOne.then(response => {
-  console.log(response)
-}).catch(error => {
-  console.err(error)
-})
 
 export default class App extends Component {
   constructor(props) {
@@ -28,49 +12,73 @@ export default class App extends Component {
       data: []
     }
   }
+  let tyrellOne = "https://anapioficeandfire.com/api/characters/16"
+let targaryenTwo = "http://www.anapioficeandfire.com/api/houses/378"
+let lannisterThree = "http://www.anapioficeandfire.com/api/houses/229"
+let baratheonFour = "http://www.anapioficeandfire.com/api/houses/17"
+let aliasFive = "http://www.anapioficeandfire.com/api/characters/901"
+let starkSix = "http://www.anapioficeandfire.com/api/houses/362"
+let booksSeven = "http://www.anapioficeandfire.com/api/characters/232"
 
-  componentDidMount() {
-    // attaching the api
-    axios.get("https://anapioficeandfire.com/api/characters/16")
-      // making a call for margaeryTyrells info
-      // got a CORS error
-      .then(res => {
-        const margaeryTyrell = res.data;
-        console.log("data ---->", margaeryTyrell)
+const requestOne = axios.get(tyrellOne);
+const requestTwo = axios.get(targaryenTwo);
+const requestThree = axios.get(lannisterThree);
+const requestFour = axios.get(baratheonFour);
+const requestFive = axios.get(aliasFive);
+const requestSix = axios.get(starkSix);
+const requestSeven = axios.get(booksSeven);
 
-        this.setState({ data: margaeryTyrell })
+axios.all([requestOne, requestTwo, requestThree]).then(axios.spread((...responses) => {
+  const responseOne = responses[0]
+  const responseTwo = responses[1]
+  const responesThree = responses[2]
+  // use/access the results 
+})).catch(errors => {
+  // react on errors.
+})
 
-      })
-      .catch(error => {
-        console.log('there is an eror', error)
-      })
+/* componentDidMount() {
+  // attaching the api
+  axios.get("https://anapioficeandfire.com/api/characters/16")
+    // making a call for margaeryTyrells info
+    // got a CORS error
+    .then(res => {
+      const margaeryTyrell = res.data;
+      console.log("data ---->", margaeryTyrell)
 
-      axios.get("http://www.anapioficeandfire.com/api/houses/378")
-      // making a call for margaeryTyrells info
-      // got a CORS error
-      .then(res => {
-        const houseTargaryen = res.data;
-        console.log("data ---->", houseTargaryen)
+      this.setState({ data: margaeryTyrell })
 
-        this.setState({ data: houseTargaryen })
+    })
+    .catch(error => {
+      console.log('there is an eror', error)
+    })
 
-      })
-      .catch(error => {
-        console.log('there is an eror', error)
-      })
+  axios.get("http://www.anapioficeandfire.com/api/houses/378")
+    // making a call for margaeryTyrells info
+    // got a CORS error
+    .then(res => {
+      const houseTargaryen = res.data;
+      console.log("data ---->", houseTargaryen)
 
-  }
+      this.setState({ data: houseTargaryen })
 
+    })
+    .catch(error => {
+      console.log('there is an eror', error)
+    })
 
-  render() {
-    return (
-      <div>
-        <h1> Game Of Thrones Characters </h1>
-        <h2> Margaery Tyrells Birthdate</h2>
-        {<ul>
-          {this.state.data.born}
-        </ul>}
-      </div>
-    )
-  }
+}
+*/
+
+render() {
+  return (
+    <div>
+      <h1> Game Of Thrones Characters </h1>
+      <h2> Margaery Tyrells Birthdate</h2>
+      {<ul>
+        {this.state.data.born}
+      </ul>}
+    </div>
+  )
+}
 }
